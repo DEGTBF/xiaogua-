bd6c603d60c266026849318f126b1824874ecb17
xiaogua <128179974+DEGTBF@users.noreply.github.com>
2024-10-21 00:45:34 +0800

    Delete automatic translation.py

--- automatic translation.py
+++
@@ -1,6 +0,0 @@
-from googletrans import Translator
-
-def automatic_translation(text, target_language='zh-CN'):
-    translator = Translator()
-    translated = translator.translate(text, dest=target_language)
-    return translated.text
